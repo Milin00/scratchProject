40e3cefc722920e0227eea9c217e04e7f2a63a51
Milin00 <wtnbryt1122@gmail.com>
2025-10-27 16:26:16 +0900

    追加

---
+++ Assets/Scripts/MoveChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class MoveChange : MonoBehaviour
+{
+    
+ 
+
+    void Start()
+    {
+
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ Assets/Scripts/MovePosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MovePosition : MonoBehaviour
+{
+    public float x;
+    public float y;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(x/100f, y/100f);
+    }
+}
